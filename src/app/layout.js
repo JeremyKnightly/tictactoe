312c0c77f689f66a3b4801a11bfa47b651eb3e2a
Jeremy Chevalier <jeremy.chevalier9494@gmail.com>
2024-06-20 10:44:52 -0600

    added title and analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <title>
+          Tic-Tac-Toe
+        </title>
+      </head>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        </body>
     </html>
   );
 }
